5e5d19c6e4ac5b855d283cd261d6863b622d0171
UnownPlain <git@unownplain.dev>
2026-02-05 22:35:42 -0500

    Add external editor support for editing manifests

--- src/environment.rs
+++ src/environment.rs
@@ -5,3 +5,9 @@ pub static CI: LazyLock<bool> =
 
 pub static VHS: LazyLock<bool> =
     LazyLock::new(|| env::var("VHS").is_ok_and(|vhs| vhs.parse() == Ok(true)));
+
+pub static EDITOR: LazyLock<Option<String>> = LazyLock::new(|| {
+    ["KOMAC_EDITOR", "EDITOR"]
+        .into_iter()
+        .find_map(|key| env::var(key).ok().filter(|val| !val.is_empty()))
+});
