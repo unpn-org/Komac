7596baf7bd72b87806bfe96d23086978be727f9b
UnownPlain <git@unownplain.dev>
2026-03-02 17:48:44 -0500

    Lowercase RelativeFilePath extensions

--- src/traits/installer_manifest.rs
+++ src/traits/installer_manifest.rs
@@ -14,7 +14,7 @@ use winget_types::{
 
 use crate::{
     match_installers::{match_installers, unmatched_installers},
-    traits::path::NormalizePath,
+    traits::path::{LowercaseExtension, NormalizePath},
 };
 
 pub trait InstallerManifestExt {
@@ -154,7 +154,7 @@ fn fix_relative_paths(
                         )
                     })
                     .map(|path| NestedInstallerFiles {
-                        relative_file_path: path.clone(),
+                        relative_file_path: path.lowercase_extension(),
                         ..nested_installer_file
                     })
             }
