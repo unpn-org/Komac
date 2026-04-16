e794cfe25c398ad8fcf42abeeff11a9a79f7b0fd
UnownPlain <git@unownplain.dev>
2026-04-15 20:23:18 -0400

    Switch to `serde-saphyr`

--- src/traits/installer_manifest.rs
+++ src/traits/installer_manifest.rs
@@ -151,13 +151,13 @@ mod tests {
     use std::collections::{BTreeSet, HashMap};
 
     use rstest::rstest;
-    use serde_yaml::to_string as to_yaml_string;
     use winget_types::installer::{
         Architecture, Installer, InstallerManifest, InstallerType, NestedInstallerFiles,
         NestedInstallerType, Scope, Switches,
     };
 
     use super::{InstallerManifestExt, merge_installer};
+    use crate::manifests::to_yaml_string;
 
     #[test]
     fn shared_url_preserves_manifest_architectures() {
@@ -284,7 +284,7 @@ mod tests {
             r#type: Some(InstallerType::Nullsoft),
             scope: Some(Scope::Machine),
             url: INSTALLER_URL.parse().unwrap(),
-            sha_256: serde_yaml::from_str(INSTALLER_SHA_256).unwrap(),
+            sha_256: serde_saphyr::from_str(INSTALLER_SHA_256).unwrap(),
             ..Installer::default()
         };
         let mut manifest = InstallerManifest {
