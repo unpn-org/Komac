b3c190ac807cb998801a30991f819f242456b8f2
UnownPlain <git@unownplain.dev>
2026-08-13 17:05:19 -0400

    Ignore URL architecture for msixbundles

--- src/download/file.rs
+++ src/download/file.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 
 use chrono::NaiveDate;
-use winget_types::Sha256String;
+use winget_types::{Sha256String, installer::Architecture};
 
 use crate::download::Download;
 
@@ -10,4 +10,10 @@ pub struct DownloadedFile {
     pub download: Download,
     pub sha_256: Sha256String,
     pub last_modified: Option<NaiveDate>,
+}
+
+impl DownloadedFile {
+    pub fn architecture(&self) -> Option<Architecture> {
+        self.download.architecture()
+    }
 }
