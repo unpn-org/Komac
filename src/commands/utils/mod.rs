d8f4b1279cf1a2f9f52520f25f79294baa0d37de
UnownPlain <git@unownplain.dev>
2026-09-15 01:44:15 -0400

    Add move package command

--- src/commands/utils/mod.rs
+++ src/commands/utils/mod.rs
@@ -1,19 +1,15 @@
-mod rate_limit;
 mod submit_option;
 
 use std::time::Duration;
 
 use color_eyre::{Result, eyre::bail};
+pub use submit_option::SubmitOption;
 use winget_types::installer::{InstallerManifest, InstallerType, NestedInstallerType};
 
+pub use crate::github::rate_limit::{RateLimit, SPINNER_SLOW_TICK_RATE};
 use crate::traits::InstallerManifestExt;
 
-pub use rate_limit::RateLimit;
-pub use submit_option::SubmitOption;
-
 pub const SPINNER_TICK_RATE: Duration = Duration::from_millis(50);
-
-pub const SPINNER_SLOW_TICK_RATE: Duration = Duration::from_millis(100);
 
 pub fn check_package_type(manifest: &InstallerManifest) -> Result<bool> {
     let (mut has_font, mut has_installer) = (false, false);
